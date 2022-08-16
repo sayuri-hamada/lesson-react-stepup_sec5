abacf0ceba9ce74e0c92d02103cd6a51dc917fe9
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-16 18:45:42 +0900

    クエリパラメータを扱う

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   //useParamsのHooksを利用してリンク元からidを受け取る
   const { id } = useParams();
+  //useLocationのHooksを利用して?以降のクエリパラメーターを受け取る
+  const { search } = useLocation();
+  //クエリパラメーターをURLSearchParamsを利用してメソッドを利用できるようにする
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterページです</h1>
       <p>パラメターは{id}です</p>
+      <p>クエリパラメターは{query.get("name")}です</p>
     </div>
   );
 };
