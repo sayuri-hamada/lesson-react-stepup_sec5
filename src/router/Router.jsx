da8426fc0c63307ca65c8d19d0752c74476b95fc
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-16 19:02:00 +0900

    404ページを用意する

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Home } from "../Home";
+import { Page404 } from "../Page404";
 import { page1Routes } from "./Page1Routes";
 import { page2Routes } from "./Page2Routes";
 
@@ -47,6 +48,10 @@ export const Router = () => {
           </Switch>
         )}
       />
+      {/** どのパスにも当てはまらなかった場合４０４ページを表示する */}
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
